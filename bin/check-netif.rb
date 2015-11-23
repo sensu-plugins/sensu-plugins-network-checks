066a42ccb932b78b3f119eae55eff27a7b2fb0c8
Autumn Wang <autumn.wang@autodesk.com>
2015-11-22 21:24:30 -0800

    Fixed rubocop check errors

--- bin/check-netif.rb
+++ bin/check-netif.rb
@@ -30,58 +30,58 @@ require 'socket'
 #
 class NetIFMetrics < Sensu::Plugin::Check::CLI
   option :interfaces,
-          description: 'Comma delimited list of interface names',
-          short: '-i INTERFACES',
-          long: '--interfaces INTERFACES',
-          default: 'eth0'
+         description: 'Comma delimited list of interface names',
+         short: '-i INTERFACES',
+         long: '--interfaces INTERFACES',
+         default: 'eth0'
 
   option :critical,
-          description: 'Critical throughput in KB',
-          short: '-c rx_kB_per_sec,tx_kB_per_sec',
-          long: '--critical rx_kB_per_sec,tx_kB_per_sec',
-          default: '1048576,1048576'
-  
+         description: 'Critical throughput in KB',
+         short: '-c rx_kb_per_sec,tx_kb_per_sec',
+         long: '--critical rx_kb_per_sec,tx_kb_per_sec',
+         default: '1048576,1048576'
+
   option :warning,
-          description: 'Waring throughput in KB',
-          short: '-w rx_kB_per_sec,tx_kB_per_sec',
-          long: '--warning rx_kB_per_sec,tx_kB_per_sec',
-          default: '104857,104857'
+         description: 'Waring throughput in KB',
+         short: '-w rx_kb_per_sec,tx_kb_per_sec',
+         long: '--warning rx_kb_per_sec,tx_kb_per_sec',
+         default: '104857,104857'
 
   def run
     warn_output = "\n"
     critical_output = "\n"
-    
+
     tmp_list = config[:critical].split(',')
     critcal_rx = tmp_list[0].to_f
     critcal_tx = tmp_list[1].to_f
-    
-    tmp_list= config[:warning].split(',')
+
+    tmp_list = config[:warning].split(',')
     warn_rx = tmp_list[0].to_f
     warn_tx = tmp_list[1].to_f
-    
+
     ifs = config[:interfaces].split(',')
-    
+
     `sar -n DEV 1 1 | grep Average | grep -v IFACE`.each_line do |line|
       stats = line.split(/\s+/)
       nic = stats[1]
 
-      next if not ifs.include?(nic)
+      next unless ifs.include?(nic)
 
-      rx_kB_per_sec = stats[4].to_f if stats[5]
-      tx_kB_per_sec = stats[5].to_f if stats[5]
-      if rx_kB_per_sec > critcal_rx
-        critical_output = critical_output + "#{nic}.rx_kB_per_sec : #{rx_kB_per_sec}\n"
-      elsif rx_kB_per_sec > warn_rx
-        warn_output = warn_output + "#{nic}.rx_kB_per_sec : #{rx_kB_per_sec}\n"
+      rx_kb_per_sec = stats[4].to_f if stats[5]
+      tx_kb_per_sec = stats[5].to_f if stats[5]
+      if rx_kb_per_sec > critcal_rx
+        critical_output += "#{nic}.rx_kb_per_sec : #{rx_kb_per_sec}\n"
+      elsif rx_kb_per_sec > warn_rx
+        warn_output += "#{nic}.rx_kb_per_sec : #{rx_kb_per_sec}\n"
       end
-      if tx_kB_per_sec > critcal_tx
-        critical_output = critical_output + "#{nic}.tx_kB_per_sec : #{tx_kB_per_sec}\n"
-      elsif tx_kB_per_sec > warn_tx
-        warn_output = warn_output + "#{nic}.tx_kB_per_sec : #{tx_kB_per_sec}\n"
+      if tx_kb_per_sec > critcal_tx
+        critical_output += "#{nic}.tx_kb_per_sec : #{tx_kb_per_sec}\n"
+      elsif tx_kb_per_sec > warn_tx
+        warn_output += "#{nic}.tx_kb_per_sec : #{tx_kb_per_sec}\n"
       end
     end
     critical critical_output if critical_output.length > 1
     warning warn_output if warn_output.length > 1
-    ok "All interface throughput is normal."
+    ok 'All interface throughput is normal.'
   end
 end
