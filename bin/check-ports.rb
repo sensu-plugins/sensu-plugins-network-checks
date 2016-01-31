da00e73a42360c86b524b2e5c2d3f845b380dd94
rtellamalla <rtellamalla@virtusa.com>
2016-01-31 16:16:08 +0530

    Updatge check-ports.rb
    Update check-ports.rb

--- bin/check-ports.rb
+++ bin/check-ports.rb
@@ -41,6 +41,13 @@ class CheckPort < Sensu::Plugin::Check::CLI
          long: '--hostname HOSTNAME',
          description: 'Host to connect to',
          default: '0.0.0.0'
+ 
+option :scheme,
+         description: 'Metric naming scheme, text to prepend to $protocol.$field',
+         long: '--scheme SCHEME',
+         short: '-s SCHEME',
+         default: 'ports.status'
+
 
   option :ports,
          short: '-p PORTS',
@@ -60,7 +67,9 @@ class CheckPort < Sensu::Plugin::Check::CLI
       TCPSocket.new(config[:host], port.to_i)
     end
     rescue Errno::ECONNREFUSED
-      critical "Connection refused by #{config[:host]}:#{port}"
+      #critical "Connection refused by #{config[:host]}:#{port}"
+     puts "#{config[:scheme]} 0 #{@timestamp} "
+    exit
     rescue Timeout::Error
       critical "Connection or read timed out (#{config[:host]}:#{port})"
     rescue Errno::EHOSTUNREACH
@@ -85,9 +94,12 @@ class CheckPort < Sensu::Plugin::Check::CLI
       okarray << 'ok' if check_port port
     end
     if okarray.size == ports.size
-      ok "All ports (#{config[:ports]}) are accessible for host #{config[:host]}"
-    else
-      critical "port count or pattern #{config[:pattern]} does not match" unless config[:crit_message]
+      puts "#{config[:scheme]} 1 #{@timestamp}"
+      exit
+      else
+      puts "#{config[:scheme]} 0 #{@timestamp} "
+      exit
+       #  critical "port count or pattern #{config[:pattern]} does not match" unless config[:crit_message]
     end
   end
 end
