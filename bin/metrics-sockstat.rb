sensu-plugins/sensu-plugins-network-checks

commit 6c7f98383c3af2d67d43df129dd76e9920366606

Update metrics-sockstat.rb

bin/metrics-sockstat.rb
@@ -35,7 +35,7 @@ class MetricsSockstat < Sensu::Plugin::Metric::CLI::Graphite
          description: 'Metric naming scheme, text to prepend to $protocol.$field',
          long: '--scheme SCHEME',
          short: '-s SCHEME',
-         default: 'network.sockets'
+         default: "#{Socket.gethostname}.network.sockets"
 
   def output_metric(name, value)
     output "#{@config[:scheme]}.#{name} #{value} #{@timestamp}"

commit e18188c90b2bba4867121ae12427baf5755e0332

Add missing require for `socket` library
Issue introduced by https://github.com/sensu-plugins/sensu-plugins-network-checks/commit/6c7f98383c3af2d67d43df129dd76e9920366606

Fixes #131

bin/metrics-sockstat.rb
@@ -28,6 +28,7 @@
 #   Released under the same terms as Sensu (the MIT license); see LICENSE for details.
 #
 require 'sensu-plugin/metric/cli'
+require 'socket'
 
 # MetricsSockstat
 class MetricsSockstat < Sensu::Plugin::Metric::CLI::Graphite